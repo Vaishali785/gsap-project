21e6dbc7be175ea813528d2f92e4baa7effb8849
Vaishali <dev.vaishali785@gmail.com>
2025-09-28 22:39:11 +0530

    created new components and added in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 // import "./App.css"
-import GsapFrom from "./practice/GsapFrom"
-import GsapFromTo from "./practice/GsapFromTo"
-import GsapScrollTrigger from "./practice/GsapScrollTrigger"
-import GsapStagger from "./practice/GsapStagger"
-import GsapTimeline from "./practice/GsapTimeline"
-import GsapTo from "./practice/GsapTo"
+import gsap from "gsap"
+import Navbar from "./components/Navbar"
+// import PracticeApp from "./practice/App"
+import { ScrollTrigger, SplitText } from "gsap/all"
+import Hero from "./components/Hero"
 
+gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
 	return (
-		<div className="flex flex-col gap-4 p-8">
-			<div className="flex gap-6">
-				<GsapTo />
-				<GsapFrom />
-				<GsapFromTo />
-			</div>
-			<div className="flex gap-6">
-				<GsapTimeline />
-				<GsapStagger />
-			</div>
-			<GsapScrollTrigger />
-		</div>
+		// <div className="flex flex-col gap-4 p-8">
+		// 	<PracticeApp />
+		// </div>
+		<main>
+			<Navbar />
+			<Hero />
+		</main>
 	)
 }
 
