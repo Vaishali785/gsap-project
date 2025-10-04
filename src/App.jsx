4cb7ae7e21f60c63b014b848f123840d460e752a
Vaishali <dev.vaishali785@gmail.com>
2025-10-04 14:33:31 +0530

    changed file from tsx to jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import "./App.css"
+import gsap from "gsap"
+import Navbar from "./components/Navbar.jsx"
+// import PracticeApp from "./practice/App"
+import { ScrollTrigger, SplitText } from "gsap/all"
+import Hero from "./components/Hero.jsx"
+import Cocktails from "./components/Cocktails.jsx"
+import About from "./components/About.jsx"
+import Art from "./components/Art.jsx"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+function App() {
+	return (
+		// <div className="flex flex-col gap-4 p-8">
+		// 	<PracticeApp />
+		// </div>
+		<main>
+			<Navbar />
+			<Hero />
+			<Cocktails />
+			<About />
+			<Art />
+		</main>
+	)
+}
+
+export default App
