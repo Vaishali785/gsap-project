68069f3056e182d023631854813b6dc0d34341fd
Vaishali <dev.vaishali785@gmail.com>
2025-10-06 14:18:28 +0530

    added new section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Hero from "./components/Hero.jsx"
 import Cocktails from "./components/Cocktails.jsx"
 import About from "./components/About.jsx"
 import Art from "./components/Art.jsx"
+import Menu from "./components/Menu.jsx"
+import Contact from "./components/Contact.jsx"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
@@ -20,6 +22,8 @@ function App() {
 			<Cocktails />
 			<About />
 			<Art />
+			<Menu />
+			<Contact />
 		</main>
 	)
 }
